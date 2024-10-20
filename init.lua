aad3dc359b20795ad7378abf2dab45ca4d860fd8
Worm-Warrior <hvoss@iastate.edu>
2024-10-20 17:35:28 -0500

    update

--- init.lua
+++ init.lua
@@ -860,7 +860,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'nordic'
+      vim.cmd.colorscheme 'naysayer'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
@@ -976,6 +976,6 @@ require('lazy').setup({
     },
   },
 })
-
+termguicolors = true
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
